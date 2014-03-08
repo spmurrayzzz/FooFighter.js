d3c3d3ca62802322abad6365fb3ac0629c8d6490
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2014-03-08 11:35:28 -0500

    dont use setInterval, this way pauses in game loop are respected

--- src/modules/GameAI.js
+++ src/modules/GameAI.js
@@ -12,10 +12,16 @@
 'use strict';
 
 function GameAI ( gameState ){
+    var d = new Date();
     this.gameState = gameState;
     this.game = gameState.game;
     this.asteroids = this.gameState.groups.asteroids;
     this.lasers = this.gameState.groups.lasers;
+    this.gameInProgress = false;
+    this.lastEnemyCreated = d.getTime();
+    this.config = {
+        enemyThrottleVal: 1000
+    };
     this.bindEvents();
 }
 
@@ -26,11 +32,30 @@ proto.bindEvents = function(){
     var vent = this.gameState.vent;
     vent.on('start', this.start.bind(this));
     vent.on('update', this.checkCollisions.bind(this));
+    vent.on('update', this.addEnemiesCheck.bind(this));
 };
 
 
 proto.start = function(){
-    setInterval(this.createAsteroid.bind(this), 1000);
+    this.gameInProgress = true;
+};
+
+
+proto.stop = function(){
+    this.gameInProgress = false;
+};
+
+
+proto.addEnemiesCheck = function(){
+    var throttleVal = this.config.enemyThrottleVal,
+        lastTime = this.lastEnemyCreated,
+        currTime = new Date().getTime();
+    if ( this.gameInProgress === true) {
+        if ( currTime - lastTime >= throttleVal ) {
+            this.createAsteroid();
+            this.lastEnemyCreated = currTime;
+        }
+    }
 };
 
 
