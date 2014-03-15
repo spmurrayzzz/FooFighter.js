90dce0a2fd4094a0f40003daea3f7c733bc751fc
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2014-03-15 09:50:10 -0400

    4 spaces

--- build/srcFiles.js
+++ build/srcFiles.js
@@ -1,15 +1,15 @@
 module.exports = [
-  'src/namespace.js',
-  'src/modules/Util.js',
-  'bower_components/shout/shout.js',
-  'src/modules/GameState.js',
-  'src/modules/GameEngine.js',
-  'src/modules/StartScreen.js',
-  'src/entities/Score.js',
-  'src/entities/Timer.js',
-  'src/entities/StarField.js',
-  'src/entities/Player.js',
-  'src/entities/EnemyShip.js',
-  'src/entities/Asteroid.js',
-  'src/app.js'
+    'src/namespace.js',
+    'src/modules/Util.js',
+    'bower_components/shout/shout.js',
+    'src/modules/GameState.js',
+    'src/modules/GameEngine.js',
+    'src/modules/StartScreen.js',
+    'src/entities/Score.js',
+    'src/entities/Timer.js',
+    'src/entities/StarField.js',
+    'src/entities/Player.js',
+    'src/entities/EnemyShip.js',
+    'src/entities/Asteroid.js',
+    'src/app.js'
 ];
