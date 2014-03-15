e988e92016e1efcaa6300ef55ae3d83ab0822f9c
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2014-03-15 10:59:27 -0400

    add messaging to buildIndex task

--- build/custom-tasks/buildIndex.js
+++ build/custom-tasks/buildIndex.js
@@ -1,20 +1,24 @@
 module.exports = function( grunt ){
     grunt.registerTask('buildIndex', function(){
         var done = this.async(),
-            fs = require('fs'),
-            fileStr;
+            fs = require('fs');
 
         fs.readFile('./build/templates/index.html', function( err, data ) {
             var content = new String(data),
-                newContent;
+                newContent,
+                msg;
 
             if ( fs.existsSync('./.production') ) {
                 newContent = content.replace('{{ srcFile }}', 'FooFighter.min.js');
+                msg = 'Built production version of index.html';
             } else {
                 newContent = content.replace('{{ srcFile }}', 'FooFighter.js');
+                msg = 'Built development version of index.html';
             }
 
             fs.writeFileSync('./index.html', newContent);
+            grunt.log.write(msg + '\n').ok();
+            done(true);
         });
 
     });
