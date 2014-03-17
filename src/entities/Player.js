5f851f2232f728e2cba81233e4a343af00c77f43
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2014-03-17 18:10:13 -0400

    define new collision rect for player sprite, should make collision detection less buggy

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -66,6 +66,7 @@ proto.create = function(){
     // Add it to the correct display group
     this.group.add(this.sprite);
     this.sprite.events.onKilled.add(this.onKill.bind(this));
+    this.sprite.body.setRectangle(83, 45, 8, 30);
     return this;
 };
 
